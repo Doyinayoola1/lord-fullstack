0259c245749a2041d0a6a0eee1696572dbbfc6b1
doyin <doyinayoola1@gmail.com>
2025-08-18 12:37:49 +0100

    changes to cloudfront

--- terraform/modules/cloudfront.tf
+++ terraform/modules/cloudfront.tf
@@ -4,9 +4,9 @@ resource "aws_cloudfront_distribution" "s3-distribution" {
     origin_access_control_id = aws_cloudfront_origin_access_control.site-oac.id
     origin_id                = "s3-site-bucket-${var.mod_environ}"
 
-    s3_origin_config {
-      origin_access_identity = "" # Not needed when using OAC
-    }
+    # s3_origin_config {
+    #   origin_access_identity = "" # Not needed when using OAC
+    # }
   }
 
 
@@ -19,14 +19,15 @@ resource "aws_cloudfront_distribution" "s3-distribution" {
     allowed_methods  = ["GET", "HEAD"]
     cached_methods   = ["GET", "HEAD"]
     target_origin_id = "s3-site-bucket-${var.mod_environ}"
+    cache_policy_id = "658327ea-f89d-4fab-a63d-7e88639e58f6" # Managed Cache Policy for HTML
 
-    forwarded_values {
-      query_string = false
+    # forwarded_values {
+    #   query_string = false
 
-      cookies {
-        forward = "none"
-      }
-    }
+    #   cookies {
+    #     forward = "none"
+    #   }
+    # }
 
     viewer_protocol_policy = "allow-all"
     min_ttl                = 0
@@ -40,21 +41,21 @@ resource "aws_cloudfront_distribution" "s3-distribution" {
     allowed_methods  = ["GET", "HEAD"]
     cached_methods   = ["GET", "HEAD"]
     target_origin_id = "s3-site-bucket-${var.mod_environ}"
+    cache_policy_id  = "658327ea-f89d-4fab-a63d-7e88639e58f6" # Managed Cache Policy for HTML
+    # forwarded_values {
+    #   query_string = false
+    #   headers      = ["Origin"]
 
-    forwarded_values {
-      query_string = false
-      headers      = ["Origin"]
-
-      cookies {
-        forward = "none"
-      }
-    }
+    #   cookies {
+    #     forward = "none"
+    #   }
+    # }
 
     min_ttl                = 0
     default_ttl            = 86400
     max_ttl                = 31536000
     compress               = true
-    viewer_protocol_policy = "redirect-to-https"
+    viewer_protocol_policy = "allow-all"
   }
 
   restrictions {
