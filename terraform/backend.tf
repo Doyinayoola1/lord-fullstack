ae958e39fce08b9d4c7d45e1650d4ec4dbd6df85
doyin <doyinayoola1@gmail.com>
2025-08-21 08:46:15 +0100

    created variable files for backend

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,10 +1,7 @@
 #This file is part of a Terraform configuration for deploying an S3 bucket with CloudFront distribution.
 terraform {
   backend "s3" {
-    bucket = "s3-state-bucket-${var.mod_environ}"
-    key    = "${var.environ}/terraform.tfstate"
-    region = var.region
-    use_lockfile = true
+    
     
   }
 }
---
+++ terraform/env/backend-production.tfvars
@@ -0,0 +1,5 @@
+region = "eu-north-1"
+bucket = "s3-state-bucket-production"
+#environ = "production"
+key = "production/terraform.tfstate"
+use_lockfile = true
